7c34ba92e8c1f6cf2c15e7b825eda0341f65826c
David Naranjo <davidnaranjo337@gmail.com>
2023-03-18 11:59:38 -0500

    clase 3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("aqui empezamos la clase 3")
+const http = require("http");
+
+const HOST = "localhost";
+const PORT = 8080;
+
+const writeHTMLResponse = (res, htmlCode) => {
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  res.end(htmlCode);
+  //res.end("Este es mi primer servidor"); //Creo instancia del servidor
+};
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  console.log("URL es: ", url);
+
+  if (url === "/other") {
+    writeHTMLResponse(res, "<p> Esta e sotra ruta </p>");
+  } else {
+    writeHTMLResponse(res, "<p> Codigo HTML </p>");
+  }
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
+});
